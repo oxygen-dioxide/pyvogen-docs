83c903e52dd767a1d51c9bab094ee5a35e246cc6
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-09-02 14:36:28 +0800

    删除ipynb中的音频数据

--- rmaudio.py
+++ rmaudio.py
@@ -1,10 +1,17 @@
 # 对所有大于1MB的ipynb文件，删除音频
 import os
 import json
+def isaudio(out):
+    return (out["output_type"]=="execute_result" and out["data"]["text/plain"]==["<IPython.lib.display.Audio object>"])
+
 for filename in os.listdir("."):
     if(filename.endswith(".ipynb")):
         if(os.path.getsize(filename)>1000000):
             print(filename)
             with open(filename,encoding="utf8") as file:
                 content=json.load(file)
-            input()
+            for cell in content["cells"]:
+                if("outputs" in cell):
+                    cell["outputs"]=[out for out in cell["outputs"] if not(isaudio(out))]
+            with open(filename,"w",encoding="utf8") as file:
+                json.dump(content,file)
